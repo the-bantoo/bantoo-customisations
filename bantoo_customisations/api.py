fde44dda36602f7df7d04a7d40d8db5cdaf9b3ec
dawoodjee <duncannkhata@gmail.com>
2021-04-27 23:05:59 +0200

    submitting lead from website form

--- bantoo_customisations/api.py
+++ bantoo_customisations/api.py
@@ -1,17 +1,33 @@
 import frappe
+import json
 from frappe import _
-from future import unicode_literals
 from frappe import utils
+from frappe.utils import getdate, nowdate
 
 @frappe.whitelist()
-def submit_appointment_request(values):
+def submit_appointment_request(args):
+    values = json.loads(args)
     
-    """frappe.throw(
-        title='Error',
-        msg='This file does not exist',
-        exc=FileNotFoundError
-    )"""
-    mess
-    frappe.msgprint("values: " + str(values))
-    frappe.errprint("values: " + str(values))
-    return True
+    date = values['date']
+    time = values['time'] #.time().strftime("%H:%M")
+    
+    if date and getdate(date) < getdate(nowdate()):
+        frappe.throw(_("Date cannot be in the past"))
+    
+    lead = frappe.new_doc("Lead")
+    lead.lead_name = values['first_name'] + " " + values['last_name']
+    lead.status = "Open"
+    lead.source = "Website"
+    lead.email_id = values['email']
+    lead.company_name = values['company']
+    lead.country = values['country']
+    lead.phone = values['phone_number']
+    lead.organization_lead = '1'
+    lead.contact_by = "tungati.m@gmail.com"
+    lead.contact_date = date + " " + time #27-04-2021 22:28 
+    
+    return lead.insert(
+        ignore_permissions=True, # ignore write permissions during insert
+        ignore_mandatory=True # insert even if mandatory fields are not set
+    )
+    
